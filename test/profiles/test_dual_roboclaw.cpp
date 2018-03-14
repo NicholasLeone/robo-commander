4ab7dfa92fef1ee5870269b98fdd51c597f6a829
Hunter Young <hunter.lw.young@gmail.com>
2018-03-14 18:53:38 -0500

    developing basic localization capabilities for both Roboclaw encoders and IMU

--- test/profiles/test_dual_roboclaw.cpp
+++ test/profiles/test_dual_roboclaw.cpp
@@ -28,6 +28,7 @@ int main(int argc, char *argv[]){
                claws.update_encoders();
                // cmds = claws.set_speeds(-0.1, 0.0);
                // claws.drive(cmds);
+               usleep(0.01 * 1000000);
           }
 
           // claws.update_encoders();
