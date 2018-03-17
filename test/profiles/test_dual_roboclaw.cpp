ac058b5c0c1f00f9f07e020a3353d60a9c92d0a7
Hunter Young <hunter.lw.young@gmail.com>
2018-03-16 20:05:19 -0500

    developing basic localization capabilities for both Roboclaw encoders and IMU

--- test/profiles/test_dual_roboclaw.cpp
+++ test/profiles/test_dual_roboclaw.cpp
@@ -22,24 +22,25 @@ int main(int argc, char *argv[]){
      if (pi >= 0){
 
           DualClaw claws(pi);
+          claws.reset_encoders();
 
-          while(1){
-               claws.update_status();
-               claws.update_encoders();
-               // cmds = claws.set_speeds(-0.1, 0.0);
-               // claws.drive(cmds);
-               usleep(0.1 * 1000000);
-          }
+          // while(1){
+          //      claws.update_status();
+          //      claws.update_encoders();
+          //      // cmds = claws.set_speeds(-0.1, 0.0);
+          //      // claws.drive(cmds);
+          //      usleep(0.1 * 1000000);
+          // }
 
-          // claws.update_encoders();
-          // cmds1 = claws.set_speeds(1.0, 0.0);
-          // cmds2 = claws.set_speeds(0.0, 0.0);
-          //
-          // claws.drive(cmds1);
-          // usleep(1.0 * 1000000);
-          // claws.drive(cmds2);
-          //
-          // claws.update_encoders();
+          claws.update_encoders();
+          cmds1 = claws.set_speeds(1.0, 0.0);
+          cmds2 = claws.set_speeds(0.0, 0.0);
+
+          claws.drive(cmds1);
+          usleep(1.0 * 1000000);
+          claws.drive(cmds2);
+
+          claws.update_encoders();
 
           pigpio_stop(pi);
      }
