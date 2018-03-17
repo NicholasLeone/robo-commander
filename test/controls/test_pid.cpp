88b5b5a534d17737af054b5458716fee6a24473e
Hunter Young <hunter.lw.young@gmail.com>
2018-03-16 22:54:52 -0500

    developing basic localization capabilities for both Roboclaw encoders and IMU

--- test/controls/test_pid.cpp
+++ test/controls/test_pid.cpp
@@ -13,12 +13,11 @@
 
 #include <pigpiod_if2.h>
 #include "actuators/dc_motor.h"
-#include "sensors/encoder.h"
+#include "sensors/imu.h"
 #include "utils/utils.h"
-#include "comms/udp.h"
 #include "pid.h"
 
-#define DEBUG_VERBOSE 1
+#define DEBUG_VERBOSE 0
 #define ENCODER_A 18
 #define ENCODER_B 23
 #define MOTOR_DIR 20
@@ -27,9 +26,7 @@
 using namespace std;
 
 static int pi;
-Encoder* enc;
 DcMotor* motor;
-UDP* rc_in;
 PID* pid1;
 PID_PARAMS pidM1params;
 
@@ -38,14 +35,12 @@ int flag_start = 0;
 static ofstream myFile;
 static int flag_exit = 0;
 
-static float maxSpd = 2.5;
+static float maxSpd = 0.;
 static float curSpeed = 0;
 static float curPos = 0;
 static int curControl = 0;
 static int maxControl = 255;
 static float targetVel = -1.0;
-
-RC_COMMAND_MSG controls;
 
 void funDummy(int s){}
 
@@ -61,21 +56,6 @@ float getControl(float curVal){
 	//cout << "PID CONTROL: " << curCtrl << endl;
 
      return curCtrl;
-}
-
-void readRC(){
-     int i = 0;
-     UDP* udp_line = rc_in;
-     RC_COMMAND_MSG* data = &controls;
-
-     char* dat = udp_line->read(sizeof(data)+4);
-
-     float vel = unpackFloat(&dat[i], &i);
-     float om = unpackFloat(&dat[i], &i);
-
-     data->speed = vel;
-     data->yaw = om;
-	targetVel = vel;
 }
 
 void funExit(int s){
@@ -94,7 +74,7 @@ void funUpdate(int s){
 	numUpdates++;
 
 	std::map<std::string, float> variables;
-     LoadInitialVariables("config.txt", variables);
+     LoadInitialVariables("/home/hunter/devel/robo-dev/config/controls/pid.config", variables);
 
      targetVel = (float) variables["target"];
      kp = (float) variables["kp"];
@@ -120,7 +100,12 @@ int main(){
      int i = 0;
      float speed;
      float pwm;
-	int encMode = ENCODER_MODE_STEP;
+	int dt;
+
+	string path = "/home/hunter/devel/robo-dev/config/sensors";
+	string file = "mpu9250";
+
+     IMU imu(path, file);
 
 	pidM1params.dt = 0.01;
 	pidM1params.max = 1;
@@ -131,7 +116,6 @@ int main(){
 	pidM1params.Ki = 1.0;
 	pidM1params.Kd = 1.0;
 
-
      attach_CtrlZ(funUpdate);
      attach_CtrlC(funExit);
 
@@ -139,20 +123,15 @@ int main(){
      cout << "Started" << endl;
 
      if(pi >= 0){
-          enc = new Encoder(pi, (int) ENCODER_A, (int) ENCODER_B, -1, 64, 90, 50, encMode, funDummy);
           motor = new DcMotor(pi,(int) MOTOR_PWM,(int) MOTOR_DIR);
 		pid1 = new PID(pidM1params);
-		rc_in = new UDP(14500,NULL);
 
-          thread firstSpd(&enc->calc_speed,enc);
-		firstSpd.detach();
-
-          motor->setSpeed(0);
+		motor->setSpeed(0);
 
           // cout << "Pi Section" << endl;
 
-          myFile.open("pid.csv");
-          myFile << "Iteration,Target Speed,Measured Speed,PWM, Error\n";
+          // myFile.open("pid.csv");
+          // myFile << "Iteration,Target Speed,Measured Speed,PWM, Error\n";
 
 		cout << "Waiting for User to start (Ctrl+Z)" << endl;
 		while(!flag_start){
@@ -166,19 +145,13 @@ int main(){
           cout << "Beginning Loop" << endl;
 
           while(1){
+			imu.update();
+			dt = imu.get_update_period();
+			usleep(dt);
+               pwm = getControl(0);
+          	// motor->setSpeed(pwm);
 
-               // cout << "Loop Section" << endl;
-			// readRC();
-			usleep(0.01 * 1000000);
-               speed = enc->getSpeed(1);
-               pwm = getControl(speed);
-
-               // if(targetVel == 0)
-               // 	motor->setSpeed(0);
-               // else
-          	motor->setSpeed(pwm);
-
-			float dutycycle = pwm * maxControl;
+			// float dutycycle = pwm * maxControl;
 
 #ifdef DEBUG_VERBOSE
                cout << "Speed, Controls, Error: " << speed/maxSpd << "		" << pwm  << "		" << pid1->_integral << endl;
@@ -196,10 +169,8 @@ int main(){
 		motor->setSpeed(0);
 		printf("LOOP EXIT\r\n");
      }
-     delete enc;
      delete motor;
 	delete pid1;
-	delete rc_in;
 
 	pigpio_stop(pi);
      return 0;
