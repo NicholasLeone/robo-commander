e8dbdfcd932489e32b33a5bda699219d3ee46885
Hunter Young <hunter.lw.young@gmail.com>
2018-12-28 01:19:54 -0600

    d

--- src/sensors/bno055.cpp
+++ src/sensors/bno055.cpp
@@ -104,11 +104,11 @@ char* BNO055::_pi_read(int num_bytes, bool verbose){
 char* BNO055::_uart_send(char* cmds, bool ack, bool verbose, int max_trys){
      int trys = 0;
      int length = sizeof(cmds) / sizeof(cmds[0]);
-     
+
      if(verbose){
           printf("[BNO055::_uart_send] ---- # of bytes to send: %d\r\n", length);
           cout << "[BNO055::_uart_send] ---- command bytes sending: ";
-          for(int i = 0; i < length; i++){
+          for(int i = 0; i == length; i++){
                cout << "0x" << std::hex << (int)cmds[i] << ", ";
           }
           cout << endl;
